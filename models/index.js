a83f152a9bb5cc5f99a0997697c587926313f38e
Vish O <v.opatha@gmail.com>
2021-09-09 20:27:38 +0930

    Update associations of models

--- models/index.js
+++ models/index.js
@@ -2,7 +2,9 @@ const Category = require('./Category');
 const Item = require('./Item');
 const User = require('./User');
 
-// TODO: Associations
+Item.hasOne(License, {
+    foreignKey: 'category_id',
+    onDelete: 'NULL',
+  });
 
-// We package our two models and export them as an object so we can import them together and use their proper names
 module.exports = { Category, Item, User };
