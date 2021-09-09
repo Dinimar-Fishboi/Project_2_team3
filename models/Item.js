156296e6a4cbbdd2395dc455ea1f264dffee0de9
Vish O <v.opatha@gmail.com>
2021-09-09 19:51:27 +0930

    Update models

--- models/Item.js
+++ models/Item.js
@@ -5,7 +5,28 @@ class Item extends Model {}
 
 Item.init(
     {
-
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              // This references the `driver` model, which we set in `Driver.js` as its `modelName` property
+              model: 'category',
+              key: 'id',
+            },
+          },
     },
     {
     sequelize,
