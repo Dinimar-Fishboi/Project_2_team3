c5b03209e54010d984efceb6218da212c91c0366
Vish O <v.opatha@gmail.com>
2021-09-14 18:26:05 +0930

    Add handlebars for home, item, search and complete homeroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,8 @@ const categoryRoutes = require('./category-routes');
 const itemRoutes = require('./item-routes');
 const userRoutes = require('./user-routes');
 
-router.use('/users', userRoutes);
+// router.use('/users', userRoutes);
 router.use('/categories', categoryRoutes);
-router.use('/items',itemRoutes);
+// router.use('/items',itemRoutes);
 
 module.exports = router;
