b4c53e67857dbe5874ec1551100ea7fe234953ff
Dinimar Fish <tayla@tallisman.com.au>
2021-09-19 11:14:03 +1000

    moved PUT and DELETE FE requests to different file to avoid conflict on Post page. Also updated backed to insert correct model name

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,36 +1,3 @@
-// const inputText = document.querySelector('#txt');
-// const myButtons = document.querySelector('.btn-list');
-// const list = document.querySelector('.container ul');
-
-// myButtons.addEventListener('click',(e)=>{
-  
-//     if(inputText.value != ""){
-//          e.preventDefault();
-
-//         const myLi = document.createElement('li');
-//         myLi.innerHTML = inputText.value;
-//         list.appendChild(myLi);
-//         var btn = document.createElement("BUTTON");
-//         btn.innerHTML = "Put";
-//         myLi.appendChild(btn);
-//         var btn2 = document.createElement("BUTTON");
-//         btn2.innerHTML = "Delete";
-//         myLi.appendChild(btn2);
-//     }
-
-//     btn2.onclick = function(evt){
-//         evt.target.parentElement.remove();
-//     }
-
-//     btn.onclick = function (evt) {
-//      window.location.href = "put.html";
-//     }
-
-//     inputText.value="";
-// })
-
-// getting the variables to post new content.
-
 async function addNewItem(event){
     event.preventDefault();
 
@@ -60,62 +27,65 @@ async function addNewItem(event){
         }
 };
 
-// Update an existing item
-async function updateItem(event){
-    event.preventDefault();
+// // Update an existing item
+// async function updateItem(event){
+//     event.preventDefault();
 
-    const title = document.getElementById('title').value.trim();
-    const description = document.getElementById('description').value.trim();
-    const category_id = document.getElementById('category').value; 
-    const itemId = document.querySelector('#mainContainer').dataset.item;
-    console.log(`itemid ${itemId}`);
+//     const title = document.getElementById('title').value.trim();
+//     const description = document.getElementById('description').value.trim();
+//     const category_id = document.getElementById('category').value; 
+//     const itemId = document.querySelector('#mainContainer').dataset.item;
+//     console.log(`itemid ${itemId}`);
+//     console.log('Title: ' + title);
+//     console.log('desc: ' + description);
+//     console.log('cat_id: ' + category_id);
 
-    if (title && description && category_id) {
-        const response = await fetch(`/api/items/${itemId}`, {
-            method: 'PATCH',
-            body: JSON.stringify({ title, description, category_id }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+//     if (title && description && category_id) {
+//         const response = await fetch(`/api/items/${itemId}`, {
+//             method: 'PUT',
+//             body: JSON.stringify({ title, description, category_id }),
+//             headers: { 'Content-Type': 'application/json' },
+//         });
     
-        if (response.ok) {
-            console.log(response);
-            document.location.replace('/userProfile');
-        } else {
-            console.log(response.statusText);
-        }
-        } else {
-            console.log("Unable to update your item.");
-        }
-};
+//         if (response.ok) {
+//             console.log(response);
+//             document.location.replace('/userProfile');
+//         } else {
+//             console.log(response.statusText);
+//         }
+//         } else {
+//             console.log("Unable to update your item.");
+//         }
+// };
 
-// Delete an existing item
-async function deleteItem(event){
-    event.preventDefault();
-    const itemId = document.querySelector('#mainContainer').dataset.item;
-    console.log(`itemid ${itemId}`);
+// // Delete an existing item
+// async function deleteItem(event){
+//     event.preventDefault();
+//     const itemId = document.querySelector('#mainContainer').dataset.item;
+//     console.log(`itemid ${itemId}`);
 
-    if (itemId) {
-        const response = await fetch(`/api/items/${itemId}`, {
-            method: 'DELETE',
-            // body: JSON.stringify({ title, description, category_id }),
-            headers: { 'Content-Type': 'application/json' },
+//     if (itemId) {
+//         const response = await fetch(`/api/items/${itemId}`, {
+//             method: 'DELETE',
+//             // body: JSON.stringify({ title, description, category_id }),
+//             headers: { 'Content-Type': 'application/json' },
             
-        });
-        console.log("afterfetch");
+//         });
+//         console.log("afterfetch");
     
-        if (response.ok) {
-            console.log(response);
-            document.location.replace('/userProfile');
-        } else {
-            console.log(response.statusText);
-        }
-        } else {
-            console.log("Unable to delete your item.");
-        }
+//         if (response.ok) {
+//             console.log(response);
+//             document.location.replace('/userProfile');
+//         } else {
+//             console.log(response.statusText);
+//         }
+//         } else {
+//             console.log("Unable to delete your item.");
+//         }
 
-}
+// }
 
-
-document.getElementById('updateBtn').addEventListener('click',updateItem);
-document.getElementById('delItemBtn').addEventListener('click',deleteItem);
+document.getElementById('postItem').addEventListener('submit', addNewItem);
+// document.getElementById('updateBtn').addEventListener('click',updateItem);
+// document.getElementById('delItemBtn').addEventListener('click',deleteItem);
 
